23f846588b6905093be28087fee48b614872ca7f
Yoshua Wuyts <i@yoshuawuyts.com>
2015-10-05 22:44:37 +1100

    deps: rm dead files

--- index.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert')
-
-module.exports = playgroundCodeGeneration
-
-// How fast can we generate new projects?
-// null -> null
-function playgroundCodeGeneration () {
-}
--- test.js
+++
@@ -1,7 +0,0 @@
-const test = require('tape')
-const playgroundCodeGeneration = require('./')
-
-test('should assert input types', function (t) {
-  t.plan(1)
-  t.throws(playgroundCodeGeneration)
-})
